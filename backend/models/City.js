6de3d7328a80b0e98c751db7dc7c78501a1343f3
rajmahal <rmahal25@gmail.com>
2018-10-05 11:05:03 -0700

    Fixed models to fit seed

--- backend/models/City.js
+++ backend/models/City.js
@@ -1,11 +1,9 @@
 const mongoose = require('../db/connection')
-Post = require('./Post')
 
 const CitySchema = new mongoose.Schema({
     cityName: String,
     country: String,
     imageUrl: String, 
-    Posts: [Post.schema]
 })
 
 mongoose.model('City', CitySchema)
--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,9 +1,11 @@
 const mongoose = require('../db/connection')
+Schema = mongoose.Schema;
 
 const PostSchema = new mongoose.Schema({
     title: String,
     description: String,
-    _user: {type: Schema.Types.ObjectId, ref: "User"}
+    _user: {type: Schema.Types.ObjectId, ref: "User"},
+    _city: {type: Schema.Types.ObjectId, ref: "City"}
 })
 
 mongoose.model('Post', PostSchema)
