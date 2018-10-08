aaf852f2cf7066c676df88ed7afca1d4af61f256
rajmahal <rmahal25@gmail.com>
2018-10-08 16:55:40 -0700

    Working on rendering posts

--- front/src/components/Dashboard/CityDetail.js
+++ front/src/components/Dashboard/CityDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Post from './Post'
 import Modal from '../Modal/Modal'
+import axios from 'axios'
 
 class CityDetail extends Component {
     constructor(props) {
@@ -24,14 +25,25 @@ class CityDetail extends Component {
         let imageUrl
         let array = []
         if(this.props.posts && this.props.choice){
-             console.log(this.props.choice)
-             
-            this.props.posts.forEach(element => {
-                 console.log(element)
-                if(element._city === this.props.choice.cityName){
-                    array.push(element)
-                }
-            });
+
+            console.log("CHOICE: "+this.props.choice)
+            console.log(this.props.choice)
+            console.log("id: "+this.props.choice._id)
+            if(this.props.choice.length !== 0){
+            axios.get('http://localhost:3001/api/posts/cities/'+this.props.choice._id)
+            .then(postResponse => {
+                console.log("POST RESPONSE:")
+                console.log(postResponse.data)
+                array = postResponse.data
+                console.log("array: "+array)
+                this.setState({
+                    posts: array
+                })
+                console.log("POSTS: "+this.state.posts)
+                // console.log("State posts:"+this.state.posts)
+            })
+        }
+            
             cityName = this.props.choice.cityName
             country = this.props.choice.country
             imageUrl = this.props.choice.imageUrl
@@ -46,12 +58,12 @@ class CityDetail extends Component {
                     <h1>{cityName}</h1>
                     <p>{country}</p>
                 </div>
+                <Post posts={array} />
                 <img src={imageUrl} alt=""/>
                 <button data-toggle="modal" data-target="#exampleModalCenter" onClick={this._onButtonClick}>
                     Create Post
                 </button>
                 {this.state.showComponent ? < Modal component={"Post"} choice={this.props.choice}/> : null}
-                <Post posts={array} />
             </div>
         )
     }
